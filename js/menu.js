07e18f64e8483def6500424265ebeb1aa8293818
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-08-21 13:02:18 +0200

    Edit

--- js/menu.js
+++ js/menu.js
@@ -11,3 +11,19 @@ menuBtnRef.setAttribute('aria-expanded', !expanded);
       mobileMenuRef.classList.toggle('is-open');
     });
   })();
+  
+// (() => {
+//   const menuOpenBtn = document.querySelector('[data-menu-open]');
+//   const menuCloseBtn = document.querySelector('[data-menu-close]');
+
+//   const mobileMenu = document.querySelector('[data-menu]');
+//   // const body = document.querySelector('body');
+
+//   menuOpenBtn.addEventListener('click', openModal);
+//   menuCloseBtn.addEventListener('click', openModal);
+
+//   function openModal() {
+//     mobileMenu.classList.toggle('is-open');
+//     // body.classList.toggle('no-scroll');
+//   }
+// })();

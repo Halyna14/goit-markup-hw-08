b42093128bb4659595d8c015a47df374eb2f3d76
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-08-23 02:21:20 +0200

    Formatting

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 // (() => {
 //     const menuBtnRef = document.querySelector("[data-menu-button]");
 //     const mobileMenuRef = document.querySelector('[data-menu]');
-  
+
 //     menuBtnRef.addEventListener("click", () => {
 //         const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-    
+
 // menuBtnRef.classList.toggle("is-open");
 // menuBtnRef.setAttribute('aria-expanded', !expanded);
 
@@ -12,21 +12,21 @@
 //     });
 //   })();
 (() => {
-  const menuOpenBtn = document.querySelector('[data-menu-open]');
-  const menuCloseBtn = document.querySelector('[data-menu-close]');
+  const menuOpenBtn = document.querySelector("[data-menu-open]");
+  const menuCloseBtn = document.querySelector("[data-menu-close]");
 
-  const mobileMenu = document.querySelector('[data-menu]');
+  const mobileMenu = document.querySelector("[data-menu]");
   // const body = document.querySelector('body');
 
-  menuOpenBtn.addEventListener('click', openModal);
-  menuCloseBtn.addEventListener('click', openModal);
+  menuOpenBtn.addEventListener("click", openModal);
+  menuCloseBtn.addEventListener("click", openModal);
 
   function openModal() {
-    mobileMenu.classList.toggle('is-open');
+    mobileMenu.classList.toggle("is-open");
     // body.classList.toggle('no-scroll');
   }
 })();
-  
+
 // (() => {
 //   const menuOpenBtn = document.querySelector('[data-menu-open]');
 //   const menuCloseBtn = document.querySelector('[data-menu-close]');
